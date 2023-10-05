11dc801497d82f2ba2bc9d507ac7cb069aefe3b5
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-10-05 16:43:34 -0500

    second commit

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,67 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var today =dayjs();
-$('#currentDay').text(today.format('MMMM D, YYYY')); 
-$(function () {
+$('#currentDay').text(today.format('dddd, MMMM D')); 
+
+var timeBlock = $(".time-block");//I hope this is the right one. YES it is the right one. 
+var currentHour = dayjs().hour();//get current time hour
+console.log(currentHour) //checking to see if the time works. 
+
+$(".time-block").each(function() {
+  var timeBlock = $(this).attr("id").split("-")[1]; //This function changes the color of the scheduler according to the current time.
+// this refers to time-block take the id and split? 
+
+if (currentHour == timeBlock) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("present"); //
+// if the current hour is EQUAL to that certain div with the current time than change it to present. 
+} else if (currentHour > timeBlock) {
+  $(this).removeClass("future");
+  $(this).addClass("past");
+ //if current hour is GREATER than the certain div with the time than it turns red. 
+ 
+} else if (currentHour < timeBlock) {
+  $(this).removeClass("present");
+  $(this).addClass("future");
+ //if current hour is LESS than the certain div with the time than it turns green.
+  } 
+});
+
+// timeBlock is equal to the class time-block in html file. It is a div that contains
+// the row with time, text box, and save button. 
+
+// var saveBttn = document.getElementsByClassName("saveBtn");
+
+// saveBttn.addEventListener('click', getInfo)
+
+// $(".saveBtn").click(function () {
+function getInfo () {
+  // var textBox = document.getElementsByClassName('description')
+  localStorage.setItem('Schedule', document.getElementsByClassName ('description')); 
+ 
+};
+
+
+//localStorage.setItem('keyName, input.value);
+// function saveInfo () {
+//   var saveText =localStorage.getItem('text');
+//   if(saveText) { 
+//     document.getElementById('description').value = saveText; 
+// } //localStorage.getItem('keyName');
+// }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+// var duration = dayjs()
+// console.log(duration)
+
+// highscoresLink.addEventListener('click', createHighscoresList);
